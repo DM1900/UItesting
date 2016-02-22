ea531c5e40603ecb529bd7554454f47aa8562eba
DM1900 <frwd00@gmail.com>
2016-02-22 21:17:48 +0000

    Slider and button
    Created slider and button on a canvas to output a value to the console.
    Manipulated an image using the slider

---
+++ UI_test_001/Assets/scripts/ButtonTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour 
+{
+	public Slider Slider;
+
+	public void DoSomething()
+	{
+		Debug.Log (Slider.value.ToString());
+
+	}
+}
